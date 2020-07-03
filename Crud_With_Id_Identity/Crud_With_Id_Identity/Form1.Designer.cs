f7e0d5cc8446aea7e2796778eb50ea0abbcbe6ad
Sheeraz Ali <sheerazali656@gmail.com>
2020-07-03 11:23:19 -0700

    Initial Commit

---
+++ Crud_With_Id_Identity/Crud_With_Id_Identity/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace Crud_With_Id_Identity
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxCompany = new System.Windows.Forms.TextBox();
+            this.textBoxPrice = new System.Windows.Forms.TextBox();
+            this.textBoxStalk = new System.Windows.Forms.TextBox();
+            this.buttonIsertData = new System.Windows.Forms.Button();
+            this.buttonUpdateData = new System.Windows.Forms.Button();
+            this.buttonDeleteData = new System.Windows.Forms.Button();
+            this.dataGridViewShowData = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxCatagory = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewShowData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(220, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Customer Detail";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(41, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(38, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 16);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Company:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(41, 215);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 16);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Price:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(41, 255);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 16);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Stalk:";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(136, 82);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(196, 20);
+            this.textBoxName.TabIndex = 0;
+            // 
+            // textBoxCompany
+            // 
+            this.textBoxCompany.Location = new System.Drawing.Point(136, 126);
+            this.textBoxCompany.Name = "textBoxCompany";
+            this.textBoxCompany.Size = new System.Drawing.Size(196, 20);
+            this.textBoxCompany.TabIndex = 1;
+            // 
+            // textBoxPrice
+            // 
+            this.textBoxPrice.Location = new System.Drawing.Point(136, 211);
+            this.textBoxPrice.Name = "textBoxPrice";
+            this.textBoxPrice.Size = new System.Drawing.Size(196, 20);
+            this.textBoxPrice.TabIndex = 3;
+            // 
+            // textBoxStalk
+            // 
+            this.textBoxStalk.Location = new System.Drawing.Point(136, 251);
+            this.textBoxStalk.Name = "textBoxStalk";
+            this.textBoxStalk.Size = new System.Drawing.Size(196, 20);
+            this.textBoxStalk.TabIndex = 4;
+            // 
+            // buttonIsertData
+            // 
+            this.buttonIsertData.Location = new System.Drawing.Point(372, 82);
+            this.buttonIsertData.Name = "buttonIsertData";
+            this.buttonIsertData.Size = new System.Drawing.Size(98, 29);
+            this.buttonIsertData.TabIndex = 5;
+            this.buttonIsertData.Text = "Insert Data";
+            this.buttonIsertData.UseVisualStyleBackColor = true;
+            this.buttonIsertData.Click += new System.EventHandler(this.buttonIsertData_Click);
+            // 
+            // buttonUpdateData
+            // 
+            this.buttonUpdateData.Location = new System.Drawing.Point(372, 149);
+            this.buttonUpdateData.Name = "buttonUpdateData";
+            this.buttonUpdateData.Size = new System.Drawing.Size(98, 29);
+            this.buttonUpdateData.TabIndex = 6;
+            this.buttonUpdateData.Text = "Update Data";
+            this.buttonUpdateData.UseVisualStyleBackColor = true;
+            this.buttonUpdateData.Click += new System.EventHandler(this.buttonUpdateData_Click);
+            // 
+            // buttonDeleteData
+            // 
+            this.buttonDeleteData.Location = new System.Drawing.Point(372, 215);
+            this.buttonDeleteData.Name = "buttonDeleteData";
+            this.buttonDeleteData.Size = new System.Drawing.Size(98, 29);
+            this.buttonDeleteData.TabIndex = 7;
+            this.buttonDeleteData.Text = "Delete Data";
+            this.buttonDeleteData.UseVisualStyleBackColor = true;
+            this.buttonDeleteData.Click += new System.EventHandler(this.buttonDeleteData_Click);
+            // 
+            // dataGridViewShowData
+            // 
+            this.dataGridViewShowData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewShowData.Location = new System.Drawing.Point(12, 299);
+            this.dataGridViewShowData.Name = "dataGridViewShowData";
+            this.dataGridViewShowData.Size = new System.Drawing.Size(513, 176);
+            this.dataGridViewShowData.TabIndex = 8;
+            this.dataGridViewShowData.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.dataGridViewShowData_MouseDoubleClick_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(40, 173);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 16);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Catagory:";
+            // 
+            // textBoxCatagory
+            // 
+            this.textBoxCatagory.Location = new System.Drawing.Point(136, 169);
+            this.textBoxCatagory.Name = "textBoxCatagory";
+            this.textBoxCatagory.Size = new System.Drawing.Size(196, 20);
+            this.textBoxCatagory.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(537, 487);
+            this.Controls.Add(this.dataGridViewShowData);
+            this.Controls.Add(this.buttonDeleteData);
+            this.Controls.Add(this.buttonUpdateData);
+            this.Controls.Add(this.buttonIsertData);
+            this.Controls.Add(this.textBoxStalk);
+            this.Controls.Add(this.textBoxPrice);
+            this.Controls.Add(this.textBoxCatagory);
+            this.Controls.Add(this.textBoxCompany);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewShowData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxCompany;
+        private System.Windows.Forms.TextBox textBoxPrice;
+        private System.Windows.Forms.TextBox textBoxStalk;
+        private System.Windows.Forms.Button buttonIsertData;
+        private System.Windows.Forms.Button buttonUpdateData;
+        private System.Windows.Forms.Button buttonDeleteData;
+        private System.Windows.Forms.DataGridView dataGridViewShowData;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxCatagory;
+    }
+}
+
---
+++ Crud_With_Id_Identity/Crud_With_Id_Identity/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Crud_With_Id_Identity
+{
+    public partial class Form1 : Form
+    {
+
+        int i;
+        string cs = ConfigurationManager.ConnectionStrings["csdb"].ConnectionString;
+
+        public Form1()
+        {
+            InitializeComponent();
+            BindGridView();
+        }
+
+        private void buttonIsertData_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(cs);
+            string query = "insert into customer values (@name, @catagory, @company, @price, @stalk)";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@name", textBoxName.Text);
+            cmd.Parameters.AddWithValue("@company", textBoxCompany.Text);
+            cmd.Parameters.AddWithValue("@catagory", textBoxCatagory.Text);
+            cmd.Parameters.AddWithValue("@price", textBoxPrice.Text);
+            cmd.Parameters.AddWithValue("@stalk", textBoxStalk.Text);
+
+            conn.Open();
+
+            int n = cmd.ExecuteNonQuery();
+
+            if (n > 0)
+            {
+
+                MessageBox.Show("Insertion Successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                BindGridView();
+                ResetControls();
+            }
+            else {
+
+
+                MessageBox.Show("Insertion Failed.", "Failure", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            conn.Close();
+
+
+
+        }
+
+        public void BindGridView() {
+
+
+            SqlConnection conn = new SqlConnection(cs);
+            string query = "select * from Customer";
+            SqlDataAdapter sda = new SqlDataAdapter(query, conn);
+            DataTable data = new DataTable();
+            sda.Fill(data);
+            dataGridViewShowData.DataSource = data;
+            dataGridViewShowData.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+        }
+
+        public void ResetControls() {
+
+            textBoxName.Clear();
+            textBoxCompany.Clear();
+            textBoxCatagory.Clear();
+            textBoxPrice.Clear();
+            textBoxStalk.Clear();
+
+        }
+
+    
+        private void dataGridViewShowData_MouseDoubleClick_1(object sender, MouseEventArgs e)
+        {
+            i = Convert.ToInt32(dataGridViewShowData.SelectedRows[0].Cells[0].Value.ToString());
+            textBoxName.Text = dataGridViewShowData.SelectedRows[0].Cells[1].Value.ToString();
+            textBoxCompany.Text = dataGridViewShowData.SelectedRows[0].Cells[2].Value.ToString();
+            textBoxCatagory.Text = dataGridViewShowData.SelectedRows[0].Cells[3].Value.ToString();
+            textBoxPrice.Text = dataGridViewShowData.SelectedRows[0].Cells[4].Value.ToString();
+            textBoxStalk.Text = dataGridViewShowData.SelectedRows[0].Cells[5].Value.ToString();
+        }
+
+        private void buttonUpdateData_Click(object sender, EventArgs e)
+        {
+
+            SqlConnection conn = new SqlConnection(cs);
+            string query = "update Customer set Name = @name, Company = @company, Catagory = @catagory, Price = @price, Stalk = @stalk where Id = @i";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@i", i);
+            cmd.Parameters.AddWithValue("@name", textBoxName.Text);
+            cmd.Parameters.AddWithValue("@company", textBoxCompany.Text);
+            cmd.Parameters.AddWithValue("@catagory", textBoxCatagory.Text);
+            cmd.Parameters.AddWithValue("@price", textBoxPrice.Text);
+            cmd.Parameters.AddWithValue("@stalk", textBoxStalk.Text);
+
+            conn.Open();
+
+            int n = cmd.ExecuteNonQuery();
+
+            if (n > 0)
+            {
+
+                MessageBox.Show("Updation Successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                BindGridView();
+                ResetControls();
+            }
+            else
+            {
+
+
+                MessageBox.Show("Updation Failed.", "Failure", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            conn.Close();
+        }
+
+        private void buttonDeleteData_Click(object sender, EventArgs e)
+        {
+
+            SqlConnection conn = new SqlConnection(cs);
+            string query = "delete from Customer where Id = @i";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@i", i);
+            conn.Open();
+
+            int n = cmd.ExecuteNonQuery();
+
+            if (n > 0)
+            {
+
+                MessageBox.Show("Deletion Successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                BindGridView();
+                ResetControls();
+            }
+            else
+            {
+
+
+                MessageBox.Show("Deletion Failed.", "Failure", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            conn.Close();
+        }
+    }
+}
